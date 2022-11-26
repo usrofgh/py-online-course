6f3c2cd8a616245d56db204a922b0f05f12fd2f7
usrofgh <mailforgimail@gmail.com>
2022-11-26 21:13:37 +0200

    small fixes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,13 @@ from math import ceil
 
 class OnlineCourse:
 
-    def __init__(self, name: str, description: str, weeks: int) -> None:
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            weeks: int
+    ) -> None:
+
         self.name = name
         self.description = description
         self.weeks = weeks
